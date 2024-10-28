168af5480dcd3426cacc0b8d02b0a4fba1da11ff
dasaro_40 <danielalesan0107@gmail.com>
2024-10-28 17:59:18 -0500

    navbar

--- mfe-carro/src/app/app.component.ts
+++ mfe-carro/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'mfe-carro';
