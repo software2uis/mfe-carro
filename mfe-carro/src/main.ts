b0f4c5169c841a6506ea3034cdb7bbe79072dd04
JorEdu33 <jorge2205561@correo.uis.edu.co>
2024-10-28 20:37:59 -0500

    remoto

--- mfe-carro/src/main.ts
+++ mfe-carro/src/main.ts
@@ -1,10 +1,2 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+import('./bootstrap')
+	.catch(err => console.error(err));
